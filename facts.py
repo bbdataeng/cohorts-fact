b1d62017fe729caa2801d29bd71625ca22112138
antocruo <antonella.cruoglio@gmail.com>
2024-12-18 15:11:11 +0100

    update main

--- facts.py
+++ facts.py
@@ -12,27 +12,31 @@ parser.add_argument(
 )  # need to add pre-existing facts and sum
 parser.add_argument("--filename", help="name of the input file")
 parser.add_argument("--alias", help="alias for the subcollection")
+parser.add_argument("--outdir", help="output directory", default= "outputs")
 parser.add_argument("--out_name", help="name of the output file")
 parser.add_argument(
     "--example", help="generates an example facts table", action="store_true"
 )
-parser.add_argument(
-    "--miabis", help="if data is in MIABIS", action="store_true"
-)
+parser.add_argument("--miabis", help="if data is in MIABIS", action="store_true")
+parser.add_argument("--orpha", help="if diagnosis is ORPHA", action="store_true")
 args = parser.parse_args()
 
 filename = args.filename
 miabis = args.miabis
 
-bio_list = pd.read_csv("documents/eu_bbmri_eric_biobanks_2024-11-25_10_05_03.csv")['Id'].tolist()
-coll_list = pd.read_csv("documents/eu_bbmri_eric_collections_2024-11-25_10_04_10.csv")['Id'].tolist()
+bio_list = pd.read_csv("documents/eu_bbmri_eric_biobanks_2024-11-25_10_05_03.csv")[
+    "Id"
+].tolist()
+coll_list = pd.read_csv("documents/eu_bbmri_eric_collections_2024-11-25_10_04_10.csv")[
+    "Id"
+].tolist()
 
 # Example facts table generation ----------------------------------------------
 if args.example:
     generate_example()
 
 else:
-    # Import data 
+    # Import data
     data = pd.read_excel(filename)
 
     # Load configuration file
@@ -52,27 +56,28 @@ else:
     else:
         out_name = collection_id.split(":")[-1]
     # Mapping ---------------------------------------------------------------------
-
+    # data['SAMPLE_PRESERVATION_MODE'] = data['SAMPLE_PRESERVATION_MODE'].str.strip()
     field_mappings = config.get("field_mappings", {})
     value_mappings = config.get("value_mappings", {})
 
-    data = mapping(data, field_mappings, value_mappings, miabis)
+
+    data = mapping(data, field_mappings, value_mappings, miabis, orpha = args.orpha)
     
     validation(data)
 
-
-    # da aggiungere controllo su campi 
+    # da aggiungere controllo su campi
 
     # Aggregation and count -------------------------------------------------------
     res = (
         data.groupby(["SEX", "DIAGNOSIS", "AGE_RANGE", "MATERIAL_TYPE"], observed=True)[
             ["PATIENT_ID", "SAMPLE_ID"]
-        ].nunique().reset_index()
+        ]
+        .nunique()
+        .reset_index()
     )
 
     # add collection id to each line in each data set according to collections
     res["COLLECTION_ID"] = collection_id
-
 
     # arrange and rename columns according to BBMRI guide
     res = res[
@@ -104,7 +109,7 @@ else:
     if args.facts:
         old_facts = pd.read_excel(args.facts)
         # concat with the pre-existent facts
-        old_facts = old_facts[ # da controllare matching colnames!!! (Attribute Names, Entity ids, XLSX)
+        old_facts = old_facts[  # da controllare matching colnames!!! (Attribute Names, Entity ids, XLSX)
             [
                 "id",
                 "collection",
@@ -133,26 +138,30 @@ else:
         )
 
         id_prex = res_merged["id"].tolist()
-    
+
         # extract id number
         existing_ids = old_facts["id"].dropna().tolist()
 
-        existing_numbers = [int(x.split(":")[-1].replace(alias, "")) for x in existing_ids]
+        existing_numbers = [
+            int(x.split(":")[-1].replace(alias, "")) for x in existing_ids
+        ]
 
     else:
         res_merged = res.copy()
 
     # Initialize the starting number for the new unique IDs
     next_id_number = 1
+
     def generate_unique_id():
         global next_id_number
         while True:
             unique_id = f"{alias}{next_id_number}"
-            new_id = f"bbmri-eric:factID:IT:collection:{collection_id}:id:{unique_id}" # add collection alias?
+            collection_id_name = alias
+            sub_id = collection_id.split(":")[-1]
+            new_id = f"bbmri-eric:factID:IT_{sub_id}:id:{unique_id}"  # add collection alias?
             if new_id not in existing_ids:
                 return new_id
             next_id_number += 1
-     
 
     # new IDs
     for i, row in res_merged.iterrows():
@@ -167,8 +176,10 @@ else:
     cols.insert(0, cols.pop(cols.index("id")))
     res_merged = res_merged[cols]
 
-    print(f"Generated {res_merged.shape[0]} combinations for {collection_id}, alias {alias}.")
+    print(
+        f"Generated {res_merged.shape[0]} combinations for {collection_id}, alias {alias}."
+    )
 
     # save to file ----------------------------------------------------------------
-    output_file = f"outputs/facts-collection-" + str(out_name) + ".xlsx"
+    output_file = f"{args.outdir}/facts-collection-" + str(out_name) + ".xlsx"
     res_merged.to_excel(output_file, index=False, sheet_name="eu_bbmri_eric_IT_facts")
